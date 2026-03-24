1f60007b9221d39b645777b62f62571ceaa3661f
shahhet958-beep <shahhet958@gmail.com>
2026-03-24 11:55:04 +0530

    Add script to calculate the sum of two numbers

---
+++ Addition of two numbers.py
@@ -0,0 +1,11 @@
+# Addition of two numbers
+
+# Taking input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the numbers
+sum = num1 + num2
+
+# Displaying the result
+print("The sum is:", sum)
